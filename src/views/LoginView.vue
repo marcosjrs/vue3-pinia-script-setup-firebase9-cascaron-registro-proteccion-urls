72d48ba6d3f488e572001cdf1942a5e93d771e72
MarcosJRS <marcosjrs80@gmail.com>
2023-04-11 17:55:28 +0200

    Vista de Login y Register usando el BaseForm y enrutado

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <BaseForm @submit="onSubmit"></BaseForm>
+</template>
+
+<script setup>
+import BaseForm from '../components/BaseForm.vue';
+import { useUserStore } from '../stores/user';
+
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+const onSubmit = async (data) => {
+  if ( await useUserStore().loginUser(data.email, data.password)) {
+    router.push({ name: 'home' });
+  }
+}
+</script>
