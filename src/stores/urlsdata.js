50e1bf4ac8bee18d69a76e7e4baf34ccdc364508
MarcosJRS <marcosjrs80@gmail.com>
2023-04-12 12:28:12 +0200

    Store con capacidad de añadir y eliminar un elemento

--- src/stores/urlsdata.js
+++ src/stores/urlsdata.js
@@ -1,8 +1,9 @@
 import { reactive, ref } from "vue";
 import { defineStore } from "pinia";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, doc, addDoc, deleteDoc } from "firebase/firestore";
 import { auth, db } from "../../firebaseConfig"
 import { useLoaderStore } from "./loader";
+import { nanoid } from "nanoid";
 
 
 export const useUrlsDataStore = defineStore("urlsDataStore", () => {
@@ -18,14 +19,40 @@ export const useUrlsDataStore = defineStore("urlsDataStore", () => {
       const querySnapshot = await getDocs(q);
       urlsData.splice(0);
       querySnapshot.forEach((doc) => {
-        urlsData.push(doc.data())
+        urlsData.push({...doc.data(), id: doc.id})
       });
-    } 
-    catch (error) {
+    } catch (error) {
       console.warn(error);
-    }finally{      
+    } finally {      
       loaderStore.inAction = false; 
     }
   }
-  return {urlsData, loadingDocs, fetchUrlsData}
+  const addUrlData = async (data) => {
+    const loaderStore = useLoaderStore();
+    try {  
+      if(!auth.currentUser.uid) return;
+      loaderStore.inAction = true;      
+      const dataToAdd = {...data, short: nanoid(6), user: auth.currentUser.uid};
+      const newUrlData = await addDoc(collection(db, "urls"), dataToAdd); 
+      urlsData.unshift({...dataToAdd, id: newUrlData.id});   
+    } catch (error) {
+      console.warn(error);
+    } finally {      
+      loaderStore.inAction = false; 
+    }
+  }
+  const deleteUrlData = async (urlData) => {
+    const loaderStore = useLoaderStore();
+    try {  
+      if(!auth.currentUser.uid) return;
+      loaderStore.inAction = true;     
+      await deleteDoc(doc(db, "urls", urlData.id)) 
+      urlsData.splice(urlsData.indexOf(urlData), 1);
+    } catch (error) {
+      console.warn(error);
+    } finally {      
+      loaderStore.inAction = false; 
+    }
+  }
+  return {urlsData, loadingDocs, fetchUrlsData, addUrlData, deleteUrlData}
 });
