76113406bd25685f290fe09eb2d9778534aa65cc
MarcosJRS <marcosjrs80@gmail.com>
2023-04-12 12:29:29 +0200

    Listado usando formulario para add nuevo y botón para eliminar existente

--- src/components/BaseUrls.vue
+++ src/components/BaseUrls.vue
@@ -1,4 +1,5 @@
 <template>
+  <urlDataForm @submit="onSubmit"></urlDataForm>
   <h3>Mis Urls <small>({{ userStore.user.value.email.toUpperCase() }})</small></h3>
   <v-list lines="one">
     <v-list-item
@@ -6,7 +7,20 @@
       :key="urlData.short"
       :title="urlData.short"
       :subtitle="urlData.name"
-    ></v-list-item>
+    >
+
+      <template v-slot:append>
+        <v-btn
+          icon="mdi-delete-outline"
+          variant="text"
+          @click="deleteItem(urlData)"
+        >
+          <v-icon>mdi-delete-outline</v-icon>
+        </v-btn>
+      </template>
+  
+  
+  </v-list-item>
   </v-list>
 </template>
 
@@ -14,11 +28,20 @@
   import { storeToRefs } from 'pinia';
   import { useUserStore } from '../stores/user';
   import { useUrlsDataStore } from '../stores/urlsdata';
+  import urlDataForm from './urlDataForm.vue';
+  
 
   const userStore = useUserStore();
   const urlsDataStore = useUrlsDataStore();
   const { urlsData } = storeToRefs(urlsDataStore);
   urlsDataStore.fetchUrlsData();
+
+  const deleteItem = (urlData) => {
+    urlsDataStore.deleteUrlData(urlData)
+  }
+  const onSubmit = async (data) => {
+    urlsDataStore.addUrlData(data);
+  }
 </script>
 
 <style scoped></style>
